3ada09e44158e90a8427734fc87fb263282fc054
sarfraz16 <30754475+sarfraz16@users.noreply.github.com>
2017-08-16 23:08:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,6 +87,16 @@ app.get('/counter', function(req, res) {
    res.send(counter.toString());
 });
 
+var names = [];
+app.get('/submit-name', function(req, res) { //URL: /submit-name?name=xxxxx
+   //Get the name from the request
+   var name = req.query.name;
+   
+   names.push(name);
+   // JSON: Java Script Object Notation
+   res.send(JSON.stringify(names));
+});
+
 app.get("/:articleName", function(req,res) {
     //articleName == article-one
     //articles[articleName] == {} content object for article-one
@@ -108,15 +118,6 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-var names = [];
-app.get('/submit-name', function(req, res) { //URL: /submit-name?name=xxxxx
-   //Get the name from the request
-   var name = req.query.name;
-   
-   names.push(name);
-   // JSON: Java Script Object Notation
-   res.send(JSON.stringify(names));
-});
 
 
 var port = 80;
